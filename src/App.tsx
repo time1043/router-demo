f7a4bad1302535cb2c514c3636d8581d5f18da17
oswin902m <1665434994@qq.com>
2026-05-19 11:46:00 +0800

    setup react-router declarative-mode (without loader)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router";
 import About from "./components/About";
+import BasicLayout from "./components/BasicLayout";
 import Home from "./components/Home";
 
 export default function App() {
-  // Problem: After refresh on about page, back to home page
-  // Need to save the information at the route
-  const [isHome, setIsHome] = useState(true);
-
   return (
-    <>
-      <nav>
-        <button type="button" onClick={() => setIsHome((isHome) => !isHome)}>
-          Toggle
-        </button>
-      </nav>
-
-      <main>{isHome ? <Home /> : <About />}</main>
-    </>
+    <Routes>
+      <Route path="/" element={<BasicLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
   );
 }
---
+++ src/components/BasicLayout.tsx
@@ -0,0 +1,35 @@
+import { Outlet, useLocation, useNavigate } from "react-router";
+
+export default function BasicLayout() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  function togglePage() {
+    if (pathname === "/") navigate("/about");
+    else navigate("/");
+  }
+
+  return (
+    <>
+      <nav>
+        <button type="button" onClick={togglePage}>
+          Toggle
+        </button>
+
+        {/* <button type="button" onClick={() => navigate("/")}>
+          Home
+        </button>
+        <button type="button" onClick={() => navigate("/about")}>
+          About
+        </button> */}
+
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">About</Link> */}
+      </nav>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
