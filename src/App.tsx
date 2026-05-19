f1df3584eb65ad3fcc1bf630ca443ba5e1fab112
oswin902m <1665434994@qq.com>
2026-05-19 10:55:55 +0800

    page map

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import About from "./components/About";
 import Home from "./components/Home";
 
+const pages = {
+  home: Home,
+  about: About,
+};
+
 export default function App() {
   // Problem: After refresh on about page, back to home page
   // Need to save the information at the route
   const [page, setPage] = useState("home");
+  const CurrentPage = pages[page];
 
   return (
     <>
@@ -15,8 +21,7 @@ export default function App() {
       </nav>
 
       <main>
-        {page === "home" && <Home />}
-        {page === "about" && <About />}
+        <CurrentPage />
       </main>
     </>
   );
